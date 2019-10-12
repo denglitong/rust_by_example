5046ad052aaf52c08d856de3d94178ec7c61ebe5
denglitong <litongdeng@gmail.com>
2019-10-12 21:28:57 +0800

    formatting
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 use std::fmt;
-use std::fmt::Formatter;
+use std::fmt::{Display, Formatter};
 
 #[derive(Debug)]
 struct Structure(i32);
@@ -81,6 +81,48 @@ impl fmt::Display for List {
         }
 
         write!(f, "]")
+    }
+}
+
+struct City {
+    name: &'static str,
+    // Latitude
+    lat: f32,
+    // Longitude
+    lon: f32,
+}
+
+impl Display for City {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        write!(
+            f,
+            "{}: {:.3}°{} {:.3}°{}",
+            self.name,
+            self.lat.abs(),
+            lat_c,
+            self.lon.abs(),
+            lon_c
+        )
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "RGB ({}, {}, {}) 0x{:02x}{:02x}{:02x}",
+            self.red, self.green, self.blue, self.red, self.green, self.blue,
+        )
     }
 }
 
@@ -181,4 +223,52 @@ fn main() {
 
     let v = List(vec![1, 2, 3]);
     println!("{}", v);
+
+    for city in [
+        City {
+            name: "Dublin",
+            lat: 53.347778,
+            lon: -6.259722,
+        },
+        City {
+            name: "Oslo",
+            lat: 59.95,
+            lon: 10.75,
+        },
+        City {
+            name: "Vancouver",
+            lat: 49.25,
+            lon: -123.1,
+        },
+    ]
+    .iter()
+    {
+        println!("{}", city);
+    }
+
+    let color_iter = [
+        Color {
+            red: 128,
+            green: 255,
+            blue: 90,
+        },
+        Color {
+            red: 0,
+            green: 3,
+            blue: 254,
+        },
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0,
+        },
+    ]
+    .iter();
+
+    for color in color_iter.clone() {
+        println!("{:?}", *color);
+    }
+    for color in color_iter {
+        println!("{}", color);
+    }
 }
