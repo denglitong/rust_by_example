b6f6d757905bde17e551c6ed454a48fc435df1f1
denglitong <litongdeng@gmail.com>
2019-10-12 21:36:35 +0800

    change dir into one project each chapter
    Signed-off-by: denglitong <litongdeng@gmail.com>

---
+++ hello_world/target/debug/deps/hello_world-08449a20efc166cb.d
@@ -0,0 +1,5 @@
+/Users/leon/workspace/rust/rust_by_example/hello_world/target/debug/deps/hello_world-08449a20efc166cb: src/main.rs
+
+/Users/leon/workspace/rust/rust_by_example/hello_world/target/debug/deps/hello_world-08449a20efc166cb.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/leon/workspace/rust/rust_by_example/hello_world/target/debug/hello_world: /Users/leon/workspace/rust/rust_by_example/hello_world/src/main.rs
