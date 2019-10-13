9ffe5986355ce96d896964b319997e1ce0f5b0c6
denglitong <litongdeng@gmail.com>
2019-10-13 21:32:14 +0800

    while let
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- flow_of_control/src/main.rs
+++ flow_of_control/src/main.rs
@@ -281,6 +281,35 @@ fn main() {
     if let Foo3::Bar = a {
         println!("a is foobar");
     }
+
+    let mut optional = Some(0);
+    loop {
+        match optional {
+            Some(i) => {
+                if i > 9 {
+                    println!("Greater than 9, quit!");
+                    optional = None;
+                } else {
+                    println!("`i` is `{:?}`. Try again.", i);
+                    optional = Some(i + 1);
+                }
+            }
+            _ => {
+                break;
+            }
+        }
+    }
+
+    let mut optional = Some(0);
+    while let Some(i) = optional {
+        if i > 9 {
+            println!("Greater than 9, quit!");
+            optional = None;
+        } else {
+            println!("`i` is `{:?}`. Try again.", i);
+            optional = Some(i + 1);
+        }
+    }
 }
 
 //#[derive(PartialEq)]
