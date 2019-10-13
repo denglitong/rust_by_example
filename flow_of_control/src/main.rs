8d188beea6909b2dad4c7672bdd4516ba1591348
denglitong <litongdeng@gmail.com>
2019-10-13 20:18:45 +0800

    loop
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- flow_of_control/src/main.rs
+++ flow_of_control/src/main.rs
@@ -18,4 +18,22 @@ fn main() {
     };
 
     println!("{} -> {}", n, big_n);
+
+    let mut count = 0u32;
+    println!("Let's count until infinity!");
+    loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three");
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("OK, that's enough");
+            break;
+        }
+    }
 }
