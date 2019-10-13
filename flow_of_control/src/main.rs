a81cc402490c6d7683e978718b648a5172796b88
denglitong <litongdeng@gmail.com>
2019-10-13 21:05:35 +0800

    structs
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- flow_of_control/src/main.rs
+++ flow_of_control/src/main.rs
@@ -203,6 +203,18 @@ fn main() {
         }
     }
     println!("mut_value: {}", mut_value); // 16
+
+    struct Foo {
+        x: (u32, u32),
+        y: u32,
+    }
+
+    let foo = Foo { x: (1, 2), y: 3 };
+    match foo {
+        Foo { x: (1, b), y } => println!("First of x is 1, b = {}, y = {}", b, y),
+        Foo { y: 2, x: i } => println!("y is 2, i = {:?}", i),
+        Foo { y, .. } => println!("y = {}, we don't care about x", y),
+    }
 }
 
 // the compiler provides a dead_code lint that will warn about unused functions,
