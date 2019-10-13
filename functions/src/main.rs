a3dd7b85069e8bfea700ac5ae3687b3feea712b1
denglitong <litongdeng@gmail.com>
2019-10-13 21:54:49 +0800

    methods
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,5 +1,81 @@
 fn main() {
     fizzbuzz(100);
+
+    let rectangle = Rectangle {
+        p1: Point::origin(),
+        p2: Point::new(3.0, 4.0),
+    };
+
+    println!("Rectangle perimeter: {}", rectangle.perimeter());
+    println!("Rectangle area: {}", rectangle.area());
+
+    let mut square = Rectangle {
+        p1: Point::origin(),
+        p2: Point::new(1.0, 1.0),
+    };
+    square.translate(2.0, 2.0);
+    println!("square: p1: {:?}, p2: {:?}", square.p1, square.p2);
+    println!("square area: {}", square.area());
+
+    let pair = Pair(Box::new(1), Box::new(2));
+    pair.destroy();
+    // pair.destroy(); // has already moved and can not use again
+}
+
+// methods are functions attached to objects
+#[derive(Debug)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+impl Point {
+    fn origin() -> Point {
+        Point { x: 0.0, y: 0.0 }
+    }
+
+    fn new(x: f64, y: f64) -> Point {
+        Point { x, y }
+    }
+}
+
+struct Rectangle {
+    p1: Point,
+    p2: Point,
+}
+
+impl Rectangle {
+    fn area(&self) -> f64 {
+        let Point { x: x1, y: y1 } = self.p1;
+        let Point { x: x2, y: y2 } = self.p2;
+        ((x1 - x2) * (y1 - y2)).abs()
+    }
+
+    fn perimeter(&self) -> f64 {
+        let Point { x: x1, y: y1 } = self.p1;
+        let Point { x: x2, y: y2 } = self.p2;
+        2.0 * ((x1 - x2).abs() + (y1 - y2).abs())
+    }
+
+    // need a mutable self
+    fn translate(&mut self, x: f64, y: f64) {
+        self.p1.x += x;
+        self.p2.x += x;
+
+        self.p1.y += y;
+        self.p2.y += y;
+    }
+}
+
+// `Pair` owns resources: two heap allocated integers
+struct Pair(Box<i32>, Box<i32>);
+
+impl Pair {
+    fn destroy(self) {
+        let Pair(first, second) = self;
+        println!("Destroying Pair({}, {})", first, second);
+        // get out of scope and get freed
+    }
 }
 
 // functions that "don't' return a value, actually return the unit type `()`
