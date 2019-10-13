6b9b922f0f4180d1a5239a09bfb0b9df56f9777f
denglitong <litongdeng@gmail.com>
2019-10-13 16:13:47 +0800

    variable bindings
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- custom_types/src/main.rs
+++ custom_types/src/main.rs
@@ -236,4 +236,18 @@ fn main() {
     println!("{} is {}", n, if is_big(n) { "Big" } else { "small" });
 
     // THRESHOLD = 5; // error, cannot modify a `const`
+
+    let an_integer = 1u32;
+    let a_boolean = true;
+    let unit = ();
+
+    // copy `an_integer` into `copied_integer`, Copy trait needed for u32
+    let copied_integer = an_integer;
+
+    println!("An integer: {:?}", copied_integer);
+    println!("A boolean: {:?}", a_boolean);
+    println!("Meet the unit value: {:?}", unit);
+
+    let _unusedd_variable = 3u32;
+    let noisy_unused_variable = 2u32;
 }
