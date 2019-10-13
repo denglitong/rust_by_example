201960431225810d15a938b7ba31bd30750f10df
denglitong <litongdeng@gmail.com>
2019-10-13 17:34:38 +0800

    To and From Strings (ToString, FromStr)
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- conversion/src/main.rs
+++ conversion/src/main.rs
@@ -1,3 +1,6 @@
+use std::fmt;
+use std::fmt::{Error, Formatter};
+
 #[derive(Debug)]
 struct Number {
     value: i32,
@@ -6,6 +9,18 @@ struct Number {
 impl From<i32> for Number {
     fn from(item: i32) -> Self {
         Number { value: item }
+    }
+}
+
+struct Circle {
+    radius: i32,
+}
+
+// implement the fmt::Display trait for type which will automagically provides ToString
+// and also allows printing the type on print!
+impl fmt::Display for Circle {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Circle of radius {}", self.radius)
     }
 }
 
@@ -23,4 +38,15 @@ fn main() {
     let int = 5;
     let num: Number = int.into();
     println!("My number is {:?}", num);
+
+    let circle = Circle { radius: 6 };
+    println!("{}", circle);
+    println!("{}", circle.to_string());
+
+    // 标准库中的数字类型实现了 FromStr trait，可以从 String 调用 parse 函数转化到对应的数字类型
+    let parsed: i32 = "5".parse().unwrap();
+    let turbo_parsed = "10".parse::<i32>().unwrap();
+
+    let sum = parsed + turbo_parsed;
+    println!("Sum: {}", sum);
 }
