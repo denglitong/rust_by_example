0c154e240919978415d2d2a6036172355c99b1b6
denglitong <litongdeng@gmail.com>
2019-10-15 23:42:01 +0800

    generics in functions
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -3,6 +3,13 @@ fn foo<T>(arg: T) {}
 
 // a concrete type `A`
 struct A;
+struct S(A);
+struct SGen<T>(T);
+
+fn reg_fn(_s: S) {}
+fn gen_spec_t(_s: SGen<A>) {}
+fn gen_spec_i32(_s: SGen<i32>) {}
+fn generic<T>(_s: SGen<T>) {}
 
 // `Single` is a concrete type
 struct Single(A);
@@ -19,4 +26,11 @@ fn main() {
     let _t = SingleGen(A); // A
     let _i32 = SingleGen(6); // i32
     let _char = SingleGen('c'); // char
+
+    reg_fn(S(A));
+    gen_spec_t(SGen(A));
+    gen_spec_i32(SGen(6));
+
+    generic(SGen('c'));
+    generic::<char>(SGen('c'));
 }
