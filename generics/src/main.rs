3240f1511210538fa5323bc24fe84935969a65b7
denglitong <litongdeng@gmail.com>
2019-10-16 00:39:35 +0800

    generics struct implementation
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -17,11 +17,40 @@ struct Single(A);
 // `SingleGen` is a generic type
 struct SingleGen<T>(T);
 
+struct CS; // concrete type
+struct GenericVal<T>(T);
+
+impl GenericVal<f32> {} // Specify f32
+impl GenericVal<CS> {} // specify CS
+
+// `<T>` must precede the type to remain generic
+impl<T> GenericVal<T> {}
+
+struct Val {
+    val: f64,
+}
+
+struct GenVal<T> {
+    gen_val: T,
+}
+
+impl Val {
+    fn value(&self) -> &f64 {
+        &self.val
+    }
+}
+
+impl<T> GenVal<T> {
+    fn value(&self) -> &T {
+        &self.gen_val
+    }
+}
+
 fn main() {
     // concrete type and explicitly takes `A`
     let _s = Single(A);
 
-     let _char: SingleGen<char> = SingleGen('a');
+    let _char: SingleGen<char> = SingleGen('a');
 
     let _t = SingleGen(A); // A
     let _i32 = SingleGen(6); // i32
@@ -33,4 +62,10 @@ fn main() {
 
     generic(SGen('c'));
     generic::<char>(SGen('c'));
+
+    let x = Val { val: 3.0 };
+    let y = GenVal { gen_val: 3i32 };
+    println!("{}, {}", x.value(), y.value());
 }
+
+// 具有 Copy trait 的 type，说明其数据存储在栈上，数据赋值给其他变量不会发生所有权的转移（其实是复制给了其他变量）
