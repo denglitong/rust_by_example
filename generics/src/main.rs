b194bf1c6ddd4c4123ae3e85a0b3cf24334f369f
denglitong <litongdeng@gmail.com>
2019-10-17 00:23:04 +0800

    associated types in trait and no need to explicitly specify all types in generics fn
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -145,8 +145,53 @@ fn main() {
 
     println!("First number: {}", container.first());
     println!("Last number: {}", container.last());
-
     println!("The difference is: {}", difference(&container));
+
+    let container = AContainer(number_1, number_2);
+
+    println!(
+        "Does container contain {} and {} : {}",
+        &number_1,
+        &number_2,
+        container.contains(&number_1, &number_2),
+    );
+
+    println!("First number: {}", container.first());
+    println!("Last number: {}", container.last());
+    println!("The difference is: {}", a_difference(&container));
+}
+
+// associated types
+struct AContainer(i32, i32);
+
+trait AContains {
+    type A;
+    type B;
+
+    fn contains(&self, _: &Self::A, _: &Self::B) -> bool;
+    fn first(&self) -> i32;
+    fn last(&self) -> i32;
+}
+
+impl AContains for AContainer {
+    type A = i32;
+    type B = i32;
+
+    fn contains(&self, number_1: &Self::A, number_2: &Self::B) -> bool {
+        &self.0 == number_1 && &self.1 == number_2
+    }
+
+    fn first(&self) -> i32 {
+        self.0
+    }
+
+    fn last(&self) -> i32 {
+        self.1
+    }
+}
+
+fn a_difference<C: AContains>(container: &C) -> i32 {
+    container.last() - container.first()
 }
 
 struct Container(i32, i32);
