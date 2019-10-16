469b683b2d88f9aa89a2c4b902b15213997d5ec0
denglitong <litongdeng@gmail.com>
2019-10-16 08:43:50 +0800

    generics bounds can apply for empty bounds even doesn't have functionality
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -99,6 +99,32 @@ fn main() {
     // print_debug(&_triangle);
     // the trait `HasArea` is not implemented for `Triangle`
     // println!("Area: {}", area(&_triangle));
+
+    let cardinal = Cardinal;
+    let blue_jay = BlueJay;
+    let _turkey = Turkey;
+
+    println!("A cardinal is {}", red(&cardinal));
+    println!("A blue jay is {}", blue(&blue_jay));
+    // println!("A turkey is {}", red(&_turkey));
+}
+
+struct Cardinal;
+struct BlueJay;
+struct Turkey;
+
+trait Red {}
+trait Blue {}
+
+impl Red for Cardinal {}
+impl Blue for BlueJay {}
+
+fn red<T: Red>(_: &T) -> &'static str {
+    "red"
+}
+
+fn blue<T: Blue>(_: &T) -> &'static str {
+    "blue"
 }
 
 struct BS<T: Display>(T);
