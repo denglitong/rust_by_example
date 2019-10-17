a1f61d2c1c41a78da10061b5deea7ae87fac220a
denglitong <litongdeng@gmail.com>
2019-10-17 21:49:19 +0800

    mutability
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- scoping_rules/src/main.rs
+++ scoping_rules/src/main.rs
@@ -36,6 +36,16 @@ fn main() {
     // this function takes ownership of the heap allocated memory from `b`
     destroy_box(b);
     // println!("b contains: {}", b); // value borrow here after move
+
+    let immutable_box = Box::new(5u32);
+    println!("immutable_box contains {}", immutable_box);
+    //*immutable_box = 4;
+
+    // move the box, changing the ownership and mutability
+    let mut mutable_box = immutable_box;
+    println!("mutable_box contains {}", mutable_box);
+    *mutable_box = 4;
+    println!("mutable_box contains {}", mutable_box);
 }
 
 fn destroy_box(c: Box<i32>) {
