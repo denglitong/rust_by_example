d8da82d9c27783a40e3c566a804ffd9a6f0677c7
denglitong <litongdeng@gmail.com>
2019-10-17 08:50:03 +0800

    RAII
    Signed-off-by: denglitong <litongdeng@gmail.com>

---
+++ scoping_rules/src/main.rs
@@ -0,0 +1,36 @@
+// RAII（Resource Acquisition Is Initialization）资源获取即初始化，
+// 或者叫作用域界定的资源管理（Scope-Bound Resource Management, SBRM）,
+// RAII是一种C++编程技术，它将必须使用前请求的资源的生命周期与一个对象的生存期绑定，
+// 保证资源可用于任何会访问该对象的函数，同时保证所有资源在其控制对象的生存周期结束时，以获取顺序的逆序释放，
+// 这有效的利用了语言特性来消除内存泄露并保证异常安全（对象生存期、退出作用域、初始化顺序以及栈回溯）
+
+// Box<T> owns memory in the heap
+
+fn main() {
+    let _box2 = Box::new(5i32);
+    {
+        let _box3 = Box::new(4i32);
+    } // `_box3` out of scope here, and will be destroyed and memory gets freed
+
+    for _ in 0u32..1_000 {
+        create_box(); // no need to manually free memory
+    }
+
+    // `_box2` will get out of scope here, and will be destroyed and memory gets freed
+
+    let _x = ToDrop;
+    println!("Made a ToDrop!");
+}
+
+struct ToDrop;
+
+impl Drop for ToDrop {
+    fn drop(&mut self) {
+        println!("ToDrop is being dropped");
+    }
+}
+
+fn create_box() {
+    // allocate an integer on the heap
+    let _box = Box::new(3i32);
+} // `_box` is destroyed here, and memory gets freed
