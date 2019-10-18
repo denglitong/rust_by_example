3bc96a5d08c7189161fa968b1b44f0312f59f33b
denglitong <litongdeng@gmail.com>
2019-10-18 19:43:02 +0800

    lifetime coercion to shortest
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- scoping_rules/src/main.rs
+++ scoping_rules/src/main.rs
@@ -227,6 +227,23 @@ fn main() {
 
     print_ref(&ref_x);
     print(ref_x);
+
+    let first = 2;
+    {
+        let second = 3;
+        println!("The product is {}", multiply(&first, &second));
+        println!("{} is the first", choose_first(&first, &second));
+    }
+}
+
+fn multiply<'a>(first: &'a i32, second: &'a i32) -> i32 {
+    first * second
+}
+
+// <'a: 'b, 'b> reads as lifetime 'a is at least as long as 'b,
+// 'a: 'b => 'a >= 'b
+fn choose_first<'a: 'b, 'b>(first: &'a i32, _: &'b i32) -> &'b i32 {
+    first
 }
 
 #[derive(Debug)]
