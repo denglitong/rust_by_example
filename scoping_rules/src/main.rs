0aa440fd564da4ae353ff6fc71f8940bb89ef4aa
denglitong <litongdeng@gmail.com>
2019-10-18 08:21:20 +0800

    function lifetime annotation constrains
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- scoping_rules/src/main.rs
+++ scoping_rules/src/main.rs
@@ -185,11 +185,47 @@ fn main() {
     failed_borrow();
     // `failed_borrow` contains no references to force `a` to be logger than the lifetime of the function,
     // but `'a` is longer. because the lifetime is never constrained, it defaults to `'static`
+
+    let x = 7;
+    let y = 9;
+
+    print_one(&x);
+    print_multi(&x, &y);
+
+    let z = pass_x(&x, &y);
+    print_one(z);
+
+    let mut t = 3;
+    add_one(&mut t);
+    print_one(&z);
+}
+
+// functions lifetimes constrains:
+// any reference must have an annotated lifetime.
+// any reference being returned must have the same lifetime as an input of be static.
+fn print_one<'a>(x: &'a i32) {
+    println!("`print_one`: x is {}", x);
+}
+
+fn add_one<'a>(x: &'a mut i32) {
+    *x += 1;
+}
+
+fn print_multi<'a, 'b>(x: &'a i32, y: &'b i32) {
+    println!("`print_multi`: x is {}, y is {}", x, y);
 }
 
 fn print_refs<'a, 'b>(x: &'a i32, y: &'b i32) {
     println!("x is {} and y is {}", x, y);
 }
+
+fn pass_x<'a, 'b>(x: &'a i32, _: &'b i32) -> &'a i32 {
+    x
+}
+
+//fn invalid_output<'a>() -> &'a String {
+//    &String::from("foo")
+//}
 
 fn failed_borrow<'a>() {
     let _x = 12;
