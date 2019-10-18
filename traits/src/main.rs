38fbb43ab3343b25acffbaf0f6e6a1a45cff1325
denglitong <litongdeng@gmail.com>
2019-10-18 20:17:45 +0800

    trait derive
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -5,7 +5,42 @@ fn main() {
     dolly.talk();
     dolly.shear();
     dolly.talk();
+
+    let _one_second = Seconds(1);
+    // println!("One second looks like: {:?}", _one_second); // no Debug trait impl for Seconds
+    // let _this_is_true = (_one_second == _one_second); // no PartialEq trait impl for Seconds
+
+    let foot = Inches(12);
+    println!("One foot equals {:?}", foot);
+
+    let meter = Centimeters(100.0);
+    let cmp = if foot.to_centimeters() < meter {
+        "smaller"
+    } else {
+        "bigger"
+    };
+    println!("One foot is {} than one meter", cmp);
 }
+
+// Eq, PartialEq, Ord, PartialOrd: comparision traits
+// Clone: to create T from &T via a copy
+// Copy: to give a type 'copy semantics' instead of 'move semantics'
+// Hash: to compute a hash from &T
+// Default: to create an empty instance of a data type
+// Debug: to format a value using the {:?} formatter
+#[derive(PartialOrd, PartialEq)]
+struct Centimeters(f64);
+
+#[derive(Debug)]
+struct Inches(i32);
+impl Inches {
+    fn to_centimeters(&self) -> Centimeters {
+        let &Inches(inches) = self;
+        Centimeters(inches as f64 * 2.54)
+    }
+}
+
+struct Seconds(i32);
 
 struct Sheep {
     naked: bool,
