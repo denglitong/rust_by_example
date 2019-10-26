a165191224971cfa0b560d3c7552abea60c97831
denglitong <litongdeng@gmail.com>
2019-10-26 09:29:41 +0800

    argument parsing simple case use matching pattern
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- std_expand_types/src/main.rs
+++ std_expand_types/src/main.rs
@@ -52,7 +52,65 @@ fn main() {
     // show_pipes();
     // show_process_wait();
     // show_fs_operation();
-    show_program_arguments();
+    // show_program_arguments();
+    show_arguments_parsing();
+}
+
+fn show_arguments_parsing() {
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        // no arguments passed
+        1 => {
+            println!("My name is 'match_args'. Try passing some arguments!");
+        }
+        2 => match args[1].parse() {
+            Ok(42) => println!("This is the answer!"),
+            _ => println!("This is not the answer."),
+        },
+        3 => {
+            let cmd = &args[1];
+            let num = &args[2];
+            let number: i32 = match num.parse() {
+                Ok(n) => n,
+                Err(_) => {
+                    eprintln!("error: second argument not an integer");
+                    help();
+                    return;
+                }
+            };
+
+            // parse the command
+            match &cmd[..] {
+                "increase" => increase(number),
+                "decrease" => decrease(number),
+                _ => {
+                    eprintln!("error: invalid command");
+                    help();
+                }
+            }
+        }
+        _ => {
+            help();
+        }
+    }
+}
+
+fn increase(number: i32) {
+    println!("{}", number + 1);
+}
+
+fn decrease(number: i32) {
+    println!("{}", number - 1);
+}
+
+fn help() {
+    println!(
+        "usage:
+match_args <string>
+    Check whether given string is the answer.
+match_args {{increase|decrease}} <integer>
+    Increase or decrease given integer by one."
+    );
 }
 
 fn show_program_arguments() {
